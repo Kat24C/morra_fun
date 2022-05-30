a05502abb55468f9182bb0fbaaa7f1588aebdeda
Kat24C <kcsurvey47@gmail.com>
2022-05-30 11:37:06 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,36 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+
+def user_details():
+    """
+    The user enter his/her name
+    As well as a breakdown of the rules of the game.
+    """
+
+
+def game_choice():
+    """
+    Here we are going to ask the user to input a number
+    between 1 and 3. 
+    The computer will randomize a number between 1 and 3. 
+    Then we will compare the two numbers to see if they 
+    are the same or different. 
+    """
+
+
+def finalgame(player_score, comp_score):
+    """
+    Here we will compare the scores and see if the game continues.
+    It will see if the computer or user has scored 12 or more.
+    It will declare a winner once user or computer score 12.
+    """
+
+
+def play_again():
+    """
+    Asks the user if they would like to restart the game.
+    The user will have the option to restart or end the game. 
+    """
+
+
+game_choice()
